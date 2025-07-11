75ab94631037cd16e1a5d7ea679896d968e92359
alakdjs <9765cass@naver.com>
2025-07-12 01:03:16 +0900

    2025-07-12 Double Linked List, Circuler Double Linked List

--- 202507101226_DoubleLinkedList_QA/DoublyLinkedList.c
+++ 202507101226_DoubleLinkedList_QA/DoublyLinkedList.c
@@ -62,15 +62,46 @@ void DLL_InsertAfter(Node* Current, Node* NewNode)
 // InsertNewHead
 void DLL_InsertNewHead(Node** Head, Node* NewHead)
 {
-    NewHead->PrevNode = NULL;
-    NewHead->NextNode = *Head;
+    // 경우 1: 헤드노드가 없는 경우
+    if ((*Head) == NULL) {
+        *Head = NewHead;
 
-    if (*Head != NULL)
-        (*Head)->PrevNode = NewHead;
+        NewHead->NextNode = NULL;
+        NewHead->PrevNode = NULL;
+    }
+    // 경우 2: 헤드노드가 있는 경우
+    else {
+        NewHead->NextNode = (*Head); // NewHead의 다음노드로 현재의 헤드노드를 연결한다.
+        (*Head)->PrevNode = NewHead;   // 현재의 헤드노드의 이전노드로 NewHead의 주소값을 연결한다.
 
-    *Head = NewHead;
+        (*Head) = NewHead; // NewHead를 헤드 노드로 만든다.
+
+        NewHead->PrevNode = NULL;
+    }
 
     NodeCount++;
+}
+
+void InsertBefore(Node** Head, Node* Current, Node* NewNode)
+{
+    // 1. Current가 현재 Head인 경우 (가장 앞에 삽입)
+    if (Current == (*Head)) {
+        // NewNode는 Head 앞에 와야 하므로, Head의 이전 노드가 됨
+        NewNode->NextNode = (*Head);     // NewNode -> Head
+        (*Head)->PrevNode = NewNode;     // Head <- NewNode
+        (*Head) = NewNode;               // Head 포인터를 NewNode로 갱신
+    }
+    else {
+        // 2. 중간 삽입: Current 앞에 삽입 (Head는 아님)
+
+        // NewNode의 연결 설정
+        NewNode->PrevNode = Current->PrevNode;  // NewNode <- Current 이전 노드
+        NewNode->NextNode = Current;            // NewNode -> Current
+
+        // 앞뒤 노드의 연결 업데이트
+        NewNode->PrevNode->NextNode = NewNode;  // 앞 노드 -> NewNode
+        NewNode->NextNode->PrevNode = NewNode;  // Current <- NewNode
+    }
 }
 
 //  노드 제거 
