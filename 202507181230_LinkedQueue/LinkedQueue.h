69c1f9634674ac2e3c89caab5ae339667f089d39
alakdjs <9765cass@naver.com>
2025-07-18 14:12:12 +0900

    2025-07-18

--- 202507181230_LinkedQueue/LinkedQueue.h
+++ 202507181230_LinkedQueue/LinkedQueue.h
@@ -13,9 +13,9 @@ typedef struct tagNode
 
 typedef struct tagLinkedQueue
 {
-    Node* Front;
-    Node* Rear;
-    int   Count;
+    Node* Front; // 맨 앞 노드를 가리킴 (dequeue 대상)
+    Node* Rear;  // 맨 뒤 노드를 가리킴 (enqueue 대상)
+    int   Count; // 노드 개수
 } LinkedQueue;
 
 void  LQ_CreateQueue(LinkedQueue** Queue);
