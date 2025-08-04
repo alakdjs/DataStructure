06e473bb6e870b6f8954d2077ac6c259856cab4b
alakdjs <9765cass@naver.com>
2025-08-04 14:04:13 +0900

    2025-08-04 Double_Linked_List-Sequential_Search

--- 202507311244_Sequential_Search/TestLinkedList.c
+++ 202507311244_Sequential_Search/TestLinkedList.c
@@ -21,32 +21,42 @@ Node* SequentialSearch(Node* Head, double Target) {
 
 // 2. 전진이동법
 // Target 매개변수에 전달된 값을 가진 노드를 찾고 해당 노드를 싱글링크드 리스트의 맨앞으로 이동시킨다(헤드노드로 만들다.)
+// 경우의 수 (찾는 값을 가지고 있는 노드의 위치)
+// 1. Head 노드인 경우
+// 2. 꼬리노드인 경우
+// 3. 이외의 경우
 Node* MoveToFront(Node** Head, double Target) {
 
-	if (*Head == NULL) {
-		return NULL;
-	}
+	Node* Current = (*Head);
+	Node* Prev = NULL;
 
-	if ((*Head)->Data.score == Target) {
-		return *Head;
-	}
+	while (Current != NULL) {
+		if (Current->Data.score == Target) { // 찾는 값을 가진 노드를 찾음
+			if (Current == (*Head)) {	 // 1. 찾는 값을 가진 노드가 헤드노드인 경우
+				return Current;
+			}
+			else if (Current->NextNode == NULL) {  // 2. 찾는 값을 가진 노드가 헤드노드인 경우
+				Prev->NextNode = NULL;			// Current의 이전노드를 꼬리노드로 만든다.
 
-	Node* Current = *Head;
+				// Current노드를 헤드노드로 만든다.
+				Current->NextNode = (*Head); // Current 다음노드로 현재의 헤드노드의 주소값을 저장
+				(*Head) = Current; // Main의 List(*Head)변수에 Current를 헤드노드의 주소값으로 갱신한다.
 
-	while (Current->NextNode != NULL) {
-		if (Current->NextNode->Data.score == Target) {
-			Node* prev = Current->NextNode;
+				return Current;
+			}
+			else {	// 3. 찾는 값을 가진 노드가 헤드도 꼬리노드도 아닌 경우
+				// Current를 링크에서 제거한다.
+				Prev->NextNode = Current->NextNode;
 
-			Current->NextNode = prev->NextNode;
+				// Current를 헤드노드로 만든다.
+				Current->NextNode = (*Head);
+				(*Head) = Current;
 
-			prev->NextNode = *Head;
-
-			*Head = prev;
-
-			return prev;
+				return Current;
+			}
 		}
-
-		Current = Current->NextNode;
+		Prev = Current;
+		Current = Current->NextNode;	// 다음 노드를 가리킨다.
 	}
 
 	return NULL;
@@ -55,49 +65,63 @@ Node* MoveToFront(Node** Head, double Target) {
 // 3. 전위법
 // Target 매개변수에 전달된 값을 가진 노드를 찾고 해당 노드를 싱글링크드 리스트에서
 // 찾는 노드를 한칸 앞쪽으로 이동시킨다.
+// 경우의 수 (찾는 값을 가지고 있는 노드의 위치)
+// 1. Head 노드인 경우
+// 2. 꼬리노드인 경우
+// 3. Head 노드 다음인 경우
+// 4. 이외의 경우
 Node* Transpose(Node** Head, double Target) {
-
-	if (*Head == NULL) {
-		return NULL;
-	}
-
-	if ((*Head)->Data.score == Target) {
-		return *Head;
-	}
-
-	if ((*Head)->NextNode != NULL && (*Head)->NextNode->Data.score == Target) {
-
-		Node* prev2 = (*Head)->NextNode;
-
-		(*Head)->NextNode = prev2->NextNode;
-		prev2->NextNode = *Head;
-		*Head = prev2;
-
-		return prev2;
-	}
-
 	Node* Current = *Head;
+	Node* Prev = NULL;
+	Node* PPrev = NULL;
 
-	while (Current->NextNode != NULL && Current->NextNode->NextNode != NULL) {
-		if (Current->NextNode->NextNode->Data.score == Target) {
+	while (Current != NULL) {
+		if (Current->Data.score == Target) {   // 찾는 값을 가진 노드를 찾음.
+			if (Current == (*Head)) {   // 1. 찾는 값을 가진 노드가 헤드노드인 경우
+				return Current;
+			}
+			else if (Current->NextNode == NULL) {   // 2. 찾는 값을 가진 노드가 꼬리노드인 경우
+				// Current를 링크에서 제거한다.
+				// Prev->NextNode = NULL;
+				Prev->NextNode = Current->NextNode;
 
-			Node* prev = Current->NextNode;
-			Node* temp = prev->NextNode;
+				// PPrev와 Prev 사이에 Current 노드를 삽입한다.
+				Current->NextNode = Prev;
+				PPrev->NextNode = Current;
 
-			prev->NextNode = temp->NextNode;
-			temp->NextNode = prev;
-			Current->NextNode = temp;
+				return Current;
+			}
+			else if ((*Head)->NextNode == Current) { // 3. Current가 헤드노드 다음 인 경우
+				// Current를 링크에서 제거한다.
+				(*Head)->NextNode = Current->NextNode;
+				// Prev->NextNode = Current->NextNode;
 
-			return temp;
+				// Current를 헤드노드로 만든다.
+				Current->NextNode = (*Head);
+				(*Head) = Current;
+
+				return Current;
+			}
+			else {   // 4. 이외의 경우
+				// Current를 링크에서 제거한다.
+				Prev->NextNode = Current->NextNode;
+
+				// Current를 PPrev(이전노드의 이전노드)와 Prev(이전노드) 사이에 삽입한다.
+				Current->NextNode = Prev;
+				PPrev->NextNode = Current;
+
+				return Current;
+			}
 
 		}
 
+		PPrev = Prev;
+		Prev = Current;
 		Current = Current->NextNode;
 	}
 
 	return NULL;
 }
-
 
 // 4. 계수법
 // Node에 빈도수(Frequency)를 저장하는 변수를 만들고
